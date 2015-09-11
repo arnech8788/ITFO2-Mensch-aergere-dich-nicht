b0289c83c06be6c23fe825ee91d1c244535e5b51
Tim <Sawatzki.Tim@gmail.com>
2015-09-11 17:58:40 +0200

    Added color typ in figure create an model package

---
+++ src/com/mensch_aergere_dich_nicht/models/Figure.java
@@ -0,0 +1,51 @@
+package com.mensch_aergere_dich_nicht.models;
+
+import java.awt.Color;
+
+/*
+ * Autor: OK / TS
+ * Datum: 11.09.2015
+ * Beschreibung: Klasse Figure
+ */
+public class Figure {
+
+	/*
+	 * color of the figure
+	 */
+	Color figureColor;
+	
+	/*
+	 * -1 = Figur ist im Start
+	 * 0 - X = Anzahl der Felder die die Figure zurückgelegt hat
+	 */
+	int iSteps;
+
+	/*
+	 * creates a figure with a specific color
+	 * sets iSteps to -1 --> home position
+	 */
+	public Figure(Color figureColor)
+	{
+		iSteps = -1;
+		this.figureColor = figureColor; 
+	}
+	
+	public void setFigureColor(Color c){
+		this.figureColor = c;
+	}
+	
+	public Color getFigureColor() {
+		return figureColor;
+	}
+	
+	public int getSteps()
+	{
+		return iSteps;
+	}
+	
+	public void setSteps(int iSteps)
+	{
+		this.iSteps = iSteps;
+	}
+
+}
