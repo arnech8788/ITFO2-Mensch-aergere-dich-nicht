6f32c011e45d85b2aa77ce49bfd2166ba518c240
Lokaler Benutzer <Lokaler Benutzer@P-143-01.Schule.107842.mts-schule.de>
2015-09-11 06:04:41 -0300

    Hammer Code

--- src/Figure.java
+++ src/Figure.java
@@ -1,4 +1,29 @@
-
+/*
+ * Autor: OK
+ * Datum: 11.09.2015
+ * Beschreibung: Klasse Figur
+ */
 public class Figure {
+	
+	int iSteps;
+	/*
+	 * -1 = Figur ist im Start
+	 * 0 - X = Anzahl der Felder die die FIgur zurückgelegt hat
+	 */
+	
+	public Figure()
+	{
+		iSteps = -1; // Figur ist zu Beginn im Start
+	}
+	
+	public int getSteps()
+	{
+		return iSteps;
+	}
+	
+	public void setSteps(int iSteps)
+	{
+		this.iSteps = iSteps;
+	}
 
 }
