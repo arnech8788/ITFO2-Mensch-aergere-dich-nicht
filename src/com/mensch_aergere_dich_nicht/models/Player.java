c983d659980a43094ba446b9c03502c8d664174d
Tim <Sawatzki.Tim@gmail.com>
2015-09-11 18:25:59 +0200

    Added variables and constructor

--- src/com/mensch_aergere_dich_nicht/models/Player.java
+++ src/com/mensch_aergere_dich_nicht/models/Player.java
@@ -1,5 +1,57 @@
 package com.mensch_aergere_dich_nicht.models;
 
-public class Player {
+import java.awt.Color;
 
+public class Player {
+	
+	Color playerColor;
+	int offset;
+	boolean isComputer;
+	String playerName;
+	Figure figures[] = new Figure[4];
+	
+	/*
+	 * Creates a player with a color, offset and name
+	 * Creates 4 figures with player color
+	 */
+	
+	public Player(Color playerColor, boolean isComputer, int offset, String playerName){
+		
+		this.playerColor = playerColor;
+		this.isComputer = isComputer;
+		this.offset = offset;
+		this.playerName = playerName;
+		
+		
+		//create 4 figures
+		for(int i = 0 ; i < 4 ; i++){
+			Figure f = new Figure(playerColor);
+			figures[i] = f;
+		}
+		
+	}
+	
+	public Color getPlayerColor(){
+		return playerColor;
+	}
+	
+	public void setPlayerName(String name){
+		playerName = name;
+	}
+	
+	public String getPlayerName(){
+		return playerName;
+	}
+	
+	public boolean isComputer(){
+		return isComputer;
+	}
+	
+	public int getOffset(){
+		return offset;
+	}
+	
+	public Figure[] getFigures(){
+		return figures;
+	}
 }
