431142eb370e5c8d3bdd68ba406621ba1ecb70e1
Arne Chudobba <Lokaler Benutzer@P-143-02.Schule.107842.mts-schule.de>
2015-09-15 10:48:56 +0200

    Anpassung Gameboard Erstellung Klasse Communication

---
+++ src/com/mensch_aergere_dich_nicht_network/NetworkCommunication.java
@@ -0,0 +1,5 @@
+package com.mensch_aergere_dich_nicht_network;
+
+public class NetworkCommunication {
+
+}
--- src/com/mensch_aergere_dich_nicht_view/Gameboard.java
+++ src/com/mensch_aergere_dich_nicht_view/Gameboard.java
@@ -1,5 +1,14 @@
 package com.mensch_aergere_dich_nicht_view;
 
-public class Gameboard {
+import javax.swing.*;
 
+public class Gameboard extends JFrame{
+
+	
+	public Gameboard(){
+		
+		this.setVisible(true);
+	}
+	
+	
 }
