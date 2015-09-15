ac538aecc2cd78cdcc2f6439aa6e14ae0a7f5d76
Lokaler Benutzer <Lokaler Benutzer@P-143-03.Schule.107842.mts-schule.de>
2015-09-15 05:55:40 -0300

    Added controls in option class

--- src/com/mensch_aergere_dich_nicht_view/Option.java
+++ src/com/mensch_aergere_dich_nicht_view/Option.java
@@ -1,25 +1,45 @@
 package com.mensch_aergere_dich_nicht_view;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Option extends JFrame{
 	
 	//Option view controls
-	JCheckBox closeGameWhenPlayerWins;
-	JCheckBox jumpInHouse;
-	JCheckBox strikeForPull;
-	JCheckBox sixFigureOut;
+	JCheckBox cbCloseGameWhenPlayerWins;
+	JCheckBox cbJumpInHouse;
+	JCheckBox cbStrikeForPull;
+	JCheckBox cbSixFigureOut;
+	JButton btnOk;
+	JButton btnClose;
+	JPanel cbPanel;
 	
 	public Option(){
 		super("Option");
 		
-		//Initialize checkboxes
-		closeGameWhenPlayerWins = new JCheckBox("Spiel beendet wenn Spieler gewonnen");
-		jumpInHouse = new JCheckBox("Im Haus überspringen");
-		strikeForPull = new JCheckBox("Schlagen geht vor ziehen");
-		sixFigureOut = new JCheckBox("Bei 6 immer raus");
+		//Initialize view controls
+		cbCloseGameWhenPlayerWins = new JCheckBox("Spiel beendet wenn Spieler gewonnen");
+		cbJumpInHouse = new JCheckBox("Im Haus überspringen");
+		cbStrikeForPull = new JCheckBox("Schlagen geht vor ziehen");
+		cbSixFigureOut = new JCheckBox("Bei 6 immer raus");
+		btnOk = new JButton("OK");
+		btnClose = new JButton("Abbrechen");
+		cbPanel = new JPanel();
 		
+		//Put checkboxes on panel control
+		cbPanel.add(cbCloseGameWhenPlayerWins);
+		cbPanel.add(cbJumpInHouse);
+		cbPanel.add(cbStrikeForPull);
+		cbPanel.add(cbSixFigureOut);
+		
+		this.add(cbPanel);
+		
+		
+		//Frame
+		this.setSize(300, 300);
+		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		this.setResizable(false);
 	}
